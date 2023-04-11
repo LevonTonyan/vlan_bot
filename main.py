f206d922bc98f21a1ddd78fc2a02990c8055ccc5
LevonTonyan <LevonTonyan87@gmail.com>
2023-04-11 20:52:52 +0400

    init

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
 import telebot
 from telebot import types
 import base64
-import time
-from fpdf import FPDF
 import requests
 import pdfkit
 
 
 bot = telebot.TeleBot("5965917042:AAEeegS2itBOoqeRebv8nUdoDedrTC6QnD0")
-pdf = FPDF()
-
 
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Please input the VIN')
+    bot.reply_to(message, f'Please input the 17 char VIN')
+
+
+# ##Getting data##############################
+# def getData(message):
+#     data = requests.get(
+#         f"https://api.allreports.tools/wp-json/v1/get_report_by_wholesaler/{message.text.upper()}/68523d5476af56837fd1b57c867f2fe9/carfax/ru")
+#     if data.status_code == 200:
+#         js = data.json()
+#         report = js["report"]["report"]
+#         decoded = base64.b64decode(report)
+#         html = decoded.decode("UTF-8")
+#         pdfkit.from_string(html, f'{js["report"]["vin"]}-{js["report"]["id"]}.pdf')
+#         carfax = open(f'./{js["report"]["vin"]}-{js["report"]["id"]}.pdf', "rb")
+#         bot.send_document(message.chat.id, carfax)
+#     else:
+#         bot.reply_to(message, data.text)
+
 
 
 @bot.message_handler()
-def choose_the_type(message):
-    markup_inline = types.InlineKeyboardMarkup()
-    item_carfax = types.InlineKeyboardButton(text="Carfax", callback_data='carfax')
-    item_autocheck = types.InlineKeyboardButton(text="Autocheck", callback_data='autocheck')
-
-    markup_inline.add(item_autocheck, item_carfax)
-    bot.send_message(message.chat.id, "Please choose the report type", reply_markup = markup_inline)
+def answer(message):
+  
+    if len(message.text) < 17:
+        bot.reply_to(message, f'Please input valid VIN code.\n Should be 17 char long!!!!')
+    else:
 
 
-
-
-@bot.callback_query_handler(func=lambda call:True)
-def answer(call):
-    if call.data == "carfax":
-        
-        data = requests.get("https://api.allreports.tools/wp-json/v1/get_report_by_wholesaler/WAUDG74F25N111998/68523d5476af56837fd1b57c867f2fe9/carfax/ru")
-        js = data.json()
-        report = js["report"]["report"]
-        decoded = base64.b64decode(report)
-        html = decoded.decode("UTF-8")
-        pdfkit.from_string(html, f'{js["report"]["vin"]}-{js["report"]["id"]}.pdf')
-        carfax =  open(f'./{js["report"]["vin"]}-{js["report"]["id"]}.pdf', "rb")
-        bot.send_document(call.message.chat.id, carfax)
-
-    elif call.data == "autocheck":
-        
-        data = requests.get("https://api.allreports.tools/wp-json/v1/get_report_by_wholesaler/WAUDG74F25N111998/68523d5476af56837fd1b57c867f2fe9/autocheck/ru")
-        js = data.json()
-        report = js["report"]["report"]
-        decoded = base64.b64decode(report)
-        html = decoded.decode("UTF-8")
-        print(html)
-        # pdfkit.from_string(html, f'{js["report"]["vin"]}-{js["report"]["id"]}.pdf')
-        # time.sleep(3)
-        # carfax =  open(f'./{js["report"]["vin"]}-{js["report"]["id"]}.pdf', "rb")
-        # bot.send_document(call.message.chat.id, carfax)
-
-
-
-
-
-
-
-
+        bot.reply_to(message, f'Please wait, checking the carfax database for records...')
+        records = requests.get(f"https://api.allreports.tools/wp-json/v1/get_report_check/{message.text}")
     
+        if records.status_code == 200:
+            markup_reply = types.InlineKeyboardMarkup()
+            get_carfax = types.InlineKeyboardButton(text="Pay and  get Carfax report", callback_data="yes")
+            markup_reply.add(get_carfax)
+
+            data = records.json()
+            bot.send_message(message.chat.id, f'Found {data["carfax"]["records"]} records in Carfax database: \n model:{data["carfax"]["vehicle"] } VIN:{data["carfax"]["vin"].upper()}', reply_markup=markup_reply)
+        else:
+            bot.reply_to(message, f'No records found for mentioned VIN!!!!')
 
 
 
 
-
-
-
+@bot.callback_query_handler(func=lambda call: True)
+def handle_answer(msg):
+        VIN = msg.message.text.split("VIN:")[1]
+        if msg.data == "yes": 
+            data = requests.get(
+                f"https://api.allreports.tools/wp-json/v1/get_report_by_wholesaler/{VIN}/68523d5476af56837fd1b57c867f2fe9/carfax/ru")
+            print(data.status_code)
+            if data.status_code == 200:
+                js = data.json()
+                report = js["report"]["report"]
+                decoded = base64.b64decode(report)
+                html = decoded.decode("UTF-8")
+                pdfkit.from_string(html, f'{js["report"]["vin"]}-{js["report"]["id"]}.pdf')
+                carfax = open(f'./{js["report"]["vin"]}-{js["report"]["id"]}.pdf', "rb")
+                bot.send_document(msg.message.json['chat']['id'], carfax)
+            else:
+                print(data.text)
 
 
 
